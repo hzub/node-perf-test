7b69ad9083fee0539d81f86b9838d058bbde34c8
Hubert Zub <hubert.zub@gmail.com>
2019-08-05 13:08:14 +0200

    Additional comment.

--- testapp/src/index.tsx
+++ testapp/src/index.tsx
@@ -2,14 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { moduleSetsFn } from './moduleSets';
 
+// Include reference to module sets so it will be surely included in the input file set
 console.info(typeof moduleSetsFn);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
